22abb0b876000b270b0c0561d90339687a5edb94
Mayzinger <32935751+Mayzinger@users.noreply.github.com>
2024-10-16 19:10:26 +0300

    Update scv.scala

--- scv.scala
+++ scv.scala
@@ -4,7 +4,7 @@ import java.nio.file.{Files, Paths, StandardCopyOption}
 
 val spark = SparkSession.builder().appName("Export Hive Table by Partition with Custom Column Names").enableHiveSupport().getOrCreate()
 
-val df = spark.sql("SELECT `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `10`, `11`, `12`, `13`, `14`, `15`, `16`, `17`, `18`, `19`, `20`, `21`, `22`, `23`, `24`, `25`, `26`, `27`, `28`, `29`, `30`, `31`, `32`, `33`, `34`, `35`, `36`, `37`, `38`, `39`, `40`, `41`, `42`, `43`, `44`, `45`, `46`, `47`, `48`, `49`, `50`, part FROM default.rb068198_all")
+val df = spark.sql("SELECT `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `10`, `11`, `12`, `13`, `14`, `15`, `16`, `17`, `18`, `19`, `20`, `21`, `22`, `23`, `24`, `25`, `26`, `27`, `28`, `29`, `30`, `31`, `32`, `33`, `34`, `35`, `36`, `37`, `38`, `39`, `40`, `41`, `42`, `43`, `44`, `45`, `46`, `47`, `48`, `49`, `50`, nttdat part FROM default.rb068198_all")
 
 val newColumnNames = Map(
   "1" -> "Дата и время транзакции",
