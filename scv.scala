6a78e1f93c91acf06ec1cb8455d8862b455456c2
Mayzinger <32935751+Mayzinger@users.noreply.github.com>
2024-10-16 16:14:50 +0300

    Update scv.scala

--- scv.scala
+++ scv.scala
@@ -12,7 +12,7 @@ val spark = SparkSession.builder()
 val hiveTableName = "my_hive_table"
 
 // Чтение данных из Hive таблицы
-val df = spark.sql(s"SELECT * FROM $hiveTableName")
+val df = spark.sql(s"SELECT `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `10`, `11`, `12`, `13`, `14`, `15`, `16`, `17`, `18`, `19`, `20`, `21`, `22`, `23`, `24`, `25`, `26`, `27`, `28`, `29`, `30`, `31`, `32`, `33`, `34`, `35`, `36`, `37`, `38`, `39`, `40`, `41`, `42`, `43`, `44`, `45`, `46`, `47`, `48`, `49`, `50` FROM default.rb068198_all")
 
 // Определяем новые названия столбцов
 val newColumnNames = Map(
