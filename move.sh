deb049a2e9a03f311b79b9290e8b289b9563072a
Mayzinger <32935751+Mayzinger@users.noreply.github.com>
2024-10-29 16:09:14 +0300

    Update move.sh

--- move.sh
+++ move.sh
@@ -1,17 +1,16 @@
 src_dir="/path/to/source"  # исходная директория с файлами и подпапками
 dest_dir="/path/to/destination"  # целевая директория
 
-# Перемещаем все файлы, добавляем суффикс при совпадении имен
-hdfs dfs -ls -R "$src_dir" | grep -v '^d' | awk '{print $8}' | while read file; do
+# Перебираем все файлы и сразу пытаемся переместить их в целевую папку
+hdfs dfs -ls -R "$src_dir" | grep -v '^d' | awk '{print $8}' | while read -r file; do
     filename=$(basename "$file")
     dest_file="$dest_dir/$filename"
 
-    # Проверяем, существует ли файл в целевой папке
-    if hdfs dfs -test -e "$dest_file"; then
-        # Добавляем суффикс времени или другой уникальный идентификатор
-        suffix=$(date +%s)
+    # Сначала пробуем переместить файл
+    if ! hdfs dfs -mv "$file" "$dest_file"; then
+        # Если возникает ошибка (например, файл с таким именем уже существует), добавляем суффикс и пробуем снова
+        suffix=$(date +%s%N)  # Уникальный суффикс с наносекундами
         dest_file="$dest_dir/${filename%.*}_$suffix.${filename##*.}"
+        hdfs dfs -mv "$file" "$dest_file"
     fi
-    
-    hdfs dfs -mv "$file" "$dest_file"
 done
